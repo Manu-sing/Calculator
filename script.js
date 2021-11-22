f0a7659a63186d4afa83391e6f8a67bcac536f37
Emanuele Guarnaccia <emanueleguarnaccia@gmail.com>
2021-11-22 18:17:11 +0800

    added a first gui but still working on stringing together several operations and get the right answer, with each pair of numbers being evaluated at a time

--- script.js
+++ script.js
@@ -8,45 +8,70 @@ const equa = document.querySelector('.equal');
 
 function add(num1, num2) {
     let add = num1 + num2;
-    return add;
+    let addR = Math.round(add * 10) / 10;
+    return addR;
 }
 
 
 function subtract(num1, num2) {
     let sub = num1 - num2;
-    return sub;
+    let subR = Math.round(sub * 10) / 10
+    return subR;
 }
 
 
 function multiply(num1, num2) {
     let mul = num1 * num2;
-    return mul;
+    let mulR = Math.round(mul * 10) / 10
+    return mulR;
 }
 
 
 function divide(num1, num2) {
-    let divi = num1 / num2;
-    return divi;
+    let divi = '';
+    let diviR = '';
+    
+    if(num1 == 0 || num2 == 0) {
+        return "Are you sure you want to divide a number by 0? ;)";
+    }
+    divi = num1 / num2;
+    diviR = Math.round(divi * 10) / 10
+    return diviR;
 }
 
 
 function operate(operator, num1, num2) {
-    operator;
-    num1;
-    num2;
-    // based on the operator, call one of the above functions on the 2 numbers
+    if(operator == '+') {
+        display.value = add(num1, num2);
+        
+    } else if(operator == '-') {
+        display.value = subtract(num1, num2);
+    
+    } else if(operator == '*') {
+        display.value = multiply(num1, num2)
+    
+    } else if(operator == '/') {
+        display.value = divide(num1, num2);
+    }
 }
 
-let displayInput = 0;
-let num1 = 0;
-let num2 = 0;
+
+function clearIt() {
+    displayInput = '';
+    display.value = displayInput;
+}
+
+
+let displayInput = '';
+let num1 = '';
+let num2 = '';
+let operator = '';
 
 buttons.forEach((button) => {
     button.addEventListener('click', function(e) {
-      
-    if(e.target.classList.contains("clear")){
-        displayInput = '';
-        display.value = displayInput;
+
+    if(e.target.classList.contains("clear")) {
+    clearIt();
     
     } else if(e.target.classList.contains("digit")) {
         display.value += e.target.innerText;
@@ -55,29 +80,23 @@ buttons.forEach((button) => {
     } else if(e.target.classList.contains("operator")) {
         num1 = displayInput;
         operator = e.target.innerText;
-        display.value = '';
-        // and then I want to clear the display so that the new digit pressed represent the new 
-        // and can then be stored in num2
-       
+        display.value = '';  
     
     } else if (e.target.classList.contains("equal")) {
-        num2 = displayInput;
-        display.value = '';
+         
         
-        if(operator == '+') {
-            display.value = add(num1, num2);
-            
-        } else if(operator == '-') {
-            display.value = subtract(num1, num2);
-        
-        } else if(operator == '*') {
-            display.value = multiply(num1, num2)
-        
-        } else if(operator == '/') {
-            display.value = divide(num1, num2);
+        if(num1 == '' && num2 == '' && operator == '')  {
+             return;
+         
+        } else {
+            num2 = displayInput;
+            display.value = '';
+            operate(operator, num1, num2);
         }
+        
+            
+        }    
     
-    }
     
     })
   });
@@ -88,6 +107,28 @@ The challenge then becomes how to handle num2... 😉
 And be careful - currently, you have it so that every time a button is clicked, you're adding another listener to the clear button. That could be... ugly.
 
 The other way you could consider might be to add one listener to all the .operator buttons, and a different listener to all the .digit buttons. Neither is really better or worse in this case, it's more about what makes sense to you. */
+
+
+/*Perfect. Do you agree that this one function is handling three pieces of logic?
+
+1) Deciding what button is pressed
+2) Mutating num1 num2 operator and displayInput variables
+3) Rendering information based on those variables
+
+Imagine if you divide the logic into separate functions, you'll have so much control on how to render data
+
+Maybe not the best suggestion, just throwing an idea out here for you:
+else if(e.target.classList.contains("operator")) {
+  if(typeof num1 === number) {
+    num2 = displayInput;
+  }
+  operator = e.target.innerText;
+  operate(operator, num1, num2)  
+}
+
+You could try adding an if statement that assigns to num2 in some way if num1 is already assigned. The provided code probably wouldn't work as you'd like, but might convey the idea to you.*/
+
+
 
 
 
